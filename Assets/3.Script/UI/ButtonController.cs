fe3355c3680f4fe0fe267f172a8ee29b403ecfa7
홍성견 <ghdtjdrus0718@naver.com>
2024-09-19 18:03:47 +0900

    [Update] CineMachine

--- Assets/3.Script/UI/ButtonController.cs
+++ Assets/3.Script/UI/ButtonController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
-public void ActiveScreen(GameObject obj)
+    public void ActiveScreen(GameObject obj)
     {
         obj.SetActive(true);
+    }
+
+    public void DisactiveScreen(GameObject obj)
+    {
+        obj.SetActive(false);
     }
 }
