7af85c112b2b3dbb3b371495fe22fedb6d92bf6e
홍성견 <ghdtjdrus0718@naver.com>
2024-10-10 18:00:15 +0900

    [Update]LevelComplete

--- Assets/3.Script/Etc/GameClearColider.cs
+++ Assets/3.Script/Etc/GameClearColider.cs
@@ -6,17 +6,50 @@ public class GameClearColider : MonoBehaviour
 {
     private OpeningDoor door;
     private Animator door_Ani;
+    private Timer timer;
+
+    public GameObject completeUI;
+    public GameObject swipeUI;
+    private Animator completeUI_Ani;
+
+    private bool isAnimationEnd = false;
 
     private void Start()
     {
         door = FindObjectOfType<OpeningDoor>();
         door_Ani = door.GetComponent<Animator>();
+        timer = FindObjectOfType<Timer>();
+        completeUI_Ani = completeUI.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (isAnimationEnd)
+        {
+            AnimatorStateInfo stateInfo = completeUI_Ani.GetCurrentAnimatorStateInfo(0);
+            if (stateInfo.normalizedTime >= 1)
+            {
+                swipeUI.SetActive(true);
+                isAnimationEnd = false;
+            }
+        }
+
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             door_Ani.SetBool("havetoOpen", false);
+            timer.DisactiveTimer();
+            Invoke("ActiveCompleteUI", 0.6f);
+            isAnimationEnd = true;
+            AnimatorStateInfo stateInfo = completeUI_Ani.GetCurrentAnimatorStateInfo(0);
+            if (stateInfo.normalizedTime >= 1) swipeUI.SetActive(true);
+
         }
+    }
+
+    private void ActiveCompleteUI()
+    {
+        completeUI.SetActive(true);
     }
 }
